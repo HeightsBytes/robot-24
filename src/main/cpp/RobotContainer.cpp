bf2ea3235f18cbfb613d52580d9474e35bc13d9d
Nathan Correa <correanathanh@gmail.com>
2024-02-22 21:11:14 -0500

    added drive and track command

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -15,6 +15,7 @@
 #include <utility>
 
 #include "commands/DefaultDrive.h"
+#include "commands/DriveAndTrack.h"
 
 RobotContainer::RobotContainer() {
 
@@ -37,33 +38,40 @@ RobotContainer::RobotContainer() {
 
 void RobotContainer::ConfigureDriverButtons() {
 
+  using enum ShooterSubsystem::State;
 
-  m_driverController.RightBumper()
-      .OnTrue(m_shooter.SetFeederCMD(-1))
-      .OnFalse(m_shooter.SetFeederCMD(0));
-
-  m_driverController.Y()
-      .OnTrue(m_shooter.SetFeederCMD(-0.25).AlongWith(m_intake.SetIntake(0.25)))
-      .OnFalse(m_shooter.SetFeederCMD(0).AlongWith(m_intake.SetIntake(0)));
-  m_driverController.B().OnTrue(m_arm.SetTargetCMD(0_deg));
-  m_driverController.X().OnTrue(
-      m_arm.SetTargetCMD(ArmConstants::Setpoint::kHandoff));
-  // m_driverController.Y().OnTrue(m_arm.SetTargetCMD(ArmConstants::Setpoint::kBlackLineShot));
-  m_driverController.A()
-      .OnTrue(m_intake.SetIntake(-1))
-      .OnFalse(m_intake.SetIntake(0));
-
-  m_dleftTrigger.OnTrue(m_intake.SetPivot(0.3)).OnFalse(m_intake.SetPivot(0));
   m_driverController.LeftBumper()
-      .OnTrue(m_intake.SetPivot(-0.3))
-      .OnFalse(m_intake.SetPivot(0));
+    .OnTrue(frc2::cmd::Parallel({m_shooter.SetTargetStateCMD(kSpeaker)}))
+    .WhileTrue(DriveAndTrack(&m_drive, 
+        [this] { return m_driverController.GetLeftY(); },
+        [this] { return m_driverController.GetLeftX(); }).ToPtr())
+    .OnFalse(frc2::cmd::Parallel({m_shooter.SetTargetStateCMD(kIdle)}));
+
+
+
+  // m_driverController.Y()
+  //     .OnTrue(m_shooter.SetFeederCMD(-0.25).AlongWith(m_intake.SetIntake(0.25)))
+  //     .OnFalse(m_shooter.SetFeederCMD(0).AlongWith(m_intake.SetIntake(0)));
+  // m_driverController.B().OnTrue(m_arm.SetTargetCMD(0_deg));
+  // m_driverController.X().OnTrue(
+  // m_arm.SetTargetCMD(ArmConstants::Setpoint::kHandoff));
+  // m_driverController.A()
+  //     .OnTrue(m_intake.SetIntake(-1))
+  //     .OnFalse(m_intake.SetIntake(0));
+
+  // m_dleftTrigger.OnTrue(m_intake.SetPivot(0.3)).OnFalse(m_intake.SetPivot(0));
+  // m_driverController.LeftBumper()
+  //     .OnTrue(m_intake.SetPivot(-0.3))
+  //     .OnFalse(m_intake.SetPivot(0));
 }
 
-void RobotContainer::ConfigureOperatorButtons() {}
+void RobotContainer::ConfigureOperatorButtons() {
+  
+}
 
 void RobotContainer::ConfigureTriggers() {
-  // frc2::RobotModeTriggers::Teleop().OnTrue(
-  //     m_shooter.SetTargetStateCMD(ShooterSubsystem::State::kStopped));
+  using enum ShooterSubsystem::State;
+  frc2::RobotModeTriggers::Teleop().OnTrue(m_shooter.SetTargetStateCMD(kIdle));
 }
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
---
+++ src/main/include/commands/DriveAndTrack.h
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#pragma once
+
+#include <frc2/command/Command.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc/controller/PIDController.h>
+
+#include <functional>
+
+#include "subsystems/DriveSubsystem.h"
+#include "subsystems/VisionSubsystem.h"
+#include "Constants.h"
+
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending Command
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class DriveAndTrack
+    : public frc2::CommandHelper<frc2::Command, DriveAndTrack> {
+ public:
+  DriveAndTrack(DriveSubsystem* drive, std::function<double()> leftY, std::function<double()> leftX);
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+
+  bool IsFinished() override;
+
+ private:
+  DriveSubsystem* m_drive;
+  VisionSubsystem& m_vision;
+  std::function<double()> m_leftY;
+  std::function<double()> m_leftX;
+  static const double kP = 0, kI = 0, kD = 0;
+  frc::PIDController m_controller{kP, kI, kD};
+};
